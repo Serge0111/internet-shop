c55d7a5ae80925c986a422ea72bc14cde884f9be
Shakhov Sergey <shakhovsergey@mail.ru>
2018-07-31 04:22:47 +0300

    finished

--- src/app/header/search-live.pipe.ts
+++ src/app/header/search-live.pipe.ts
@@ -8,12 +8,14 @@ export class SearchLivePipe implements PipeTransform {
   transform(products: any, args?: any): any {
     let product = 0;
     let str = args[0] || '';
-    str = str.replace(/^\s*/, '').replace(/\s*$/, '');
+    str = str.replace(/^\s*/, '').replace(/\s*$/, '').toLowerCase();
     if (args[1] === 'all') {
       product = products.filter( field =>
-        String(field.brand).indexOf(str) !== -1 || String(field.item).indexOf(str) !== -1 || String(field.price).indexOf(str) !== -1);
+        String(field.brand).toLowerCase().indexOf(str) !== -1 ||
+        String(field.item).toLowerCase().indexOf(str) !== -1 ||
+        String(field.price).toLowerCase().indexOf(str) !== -1);
     } else {
-      product = products.filter( field => String(field[args[1]]).indexOf(str) !== -1 );
+      product = products.filter( field => String(field[args[1]]).toLowerCase().indexOf(str) !== -1 );
     }
     return str ? product :  null;
   }
